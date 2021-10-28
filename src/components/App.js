4b18242d43fb3660b6ec1201a2029d3861cba44e
John <johnarkas679@gmail.com>
2021-10-28 22:32:45 +0300

    navbar , new post ,  post lists

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,9 @@ import RedSparrow from '../abis/RedSparrow.json'
 
 import ReactDOM from "react-dom";
 
+import { Navbar , Container, Row , Col, Button , Card , Modal , Form } from 'react-bootstrap';
+
+import Logo from './favicon.ico'
 class App extends Component {
 
   async loadWeb3() {
@@ -43,6 +46,7 @@ class App extends Component {
       // console.log(abi)
       // console.log(contract);
       let posts =  await contract.methods.GetPosts.call()
+      this.setState({posts: posts})
       console.log(posts)
     }else{
       alert("error")
@@ -53,19 +57,158 @@ class App extends Component {
   constructor(props){
     super(props)
     
+    this.ClosePopup = this.ClosePopup.bind(this)
+    this.onChangeText = this.onChangeText.bind(this)
+
     this.state ={
       
       account: '',
       contract: null,
       posts : [] ,
       comments: [],
+      ClosePopup:false,
+      text:""
     }
   }
+
+  LoadPosts(){
+
+    return this.state.posts.map((post) => {
+            
+      return(
+          <div style={{}}>
+
+              <Card style={{width:"500px", marginLeft:'200px'}}>
+                <Card.Body style={{marginLeft:'30px'}}>
+                    <Card.Title style={{}}>
+                        {post.Username}
+                    </Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">{post.User}</Card.Subtitle>
+                    <Card.Text>
+                      {post.Text}
+                    </Card.Text>
+                </Card.Body>
+              </Card>
+              <br/>
+          </div>
+
+        
+      );
+    });
+  }
+  NavBar(){
+    
+      return(
+        <Navbar bg="dark">
+        <Container>
+          <Navbar.Brand href="#home">
+            <Row>
+              <div>
+
+                
+              </div>
+              <Col>
+                <img
+                  src={Logo}
+                  width="30"
+                  height="30"
+                  className="d-inline-block align-top"
+                  alt="React Bootstrap logo"
+                />
+              </Col>
+              <Col style={{color:'red'}}>
+                Red Sparrow
+              </Col>
+              <Col>
+                <button  style={{
+                    textAlign:'right' , 
+                    background:'transparent', 
+                    border:'transparent', 
+                    color: 'white'
+                  }} 
+                  onClick={()=>{ this.setState({popup:true})} }
+                >New Post</button>
+              </Col>
+            </Row>
+            
+          </Navbar.Brand>
+        </Container>
+        </Navbar>
+      )
+  }
+
+  onChangeText(e){
+    this.setState({
+      text: e.target.value
+    });
+  }
+  ClosePopup(e){
+    e.preventDefault();
+    this.setState({popup:false})
+    this.setState({
+      
+      text:"",
+    })
+}
+
+  AddPost(props){
+    return(
+        <Modal
+        {...props}
+        
+        show={this.state.popup}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header>
+          <Modal.Title id="contained-modal-title-vcenter">
+            New Post
+          </Modal.Title>
+        </Modal.Header>
+            <Modal.Body>
+                <Form  onSubmit={this.onSubmit}  style={{textAlign:"center", width:"95%"}} >
+                    
+                        <textarea
+                            
+                            id="model"
+                            rows={3}
+                            placeholder="share your thoughts"
+                            required="required" 
+                            className="form-control"
+                            value={this.state.text}
+                            onChange={this.onChangeText}
+                        />
+                      
+                      <br/>
+                        
+
+                        <Row>
+                          <Col>
+                            <Button id="editsubmitbutton" onClick={this.ClosePopup}>
+                                Close
+                            </Button>
+                          </Col>
+                          <Col>
+                              <Button variant="primary" id="editsubmitbutton" type="submit">
+                                  Post
+                              </Button>
+                          </Col>
+                        </Row>
+                        
+                    </Form>
+        </Modal.Body>
+      </Modal>
+      );
+}
 
   render() {
     return (
       <div>
-       
+        {this.NavBar()}
+        <br/>
+        {this.AddPost()}
+        {this.LoadPosts()}
       </div>
     );
   }
